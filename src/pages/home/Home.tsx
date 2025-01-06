d144116d90ba71de963bdb0c0fe552499a925720
칼있으마 <bok-yeol.jung@doosan.com>
2025-01-06 12:24:52 +0900

    1:21:47 홈페이지를 만들었고 이제 다른 메뉴에 해당하는 페이지를 만들어야함

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import { BarChartBox } from '../../components/barChartBox/BarChartBox';
+import { BigChartBox } from '../../components/bigChartBox/BigChartBox';
 import { ChartBox } from '../../components/chartBox/ChartBox';
 import { PieChartBox } from '../../components/pieChartBox/PieChartBox';
 import { TopBox } from '../../components/topBox/TopBox';
@@ -21,7 +22,7 @@ export const Home = () => {
       <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
-      <div className="box box7">Box7</div>
+      <div className="box box7"><BigChartBox/></div>
       <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
       <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
     </div>
