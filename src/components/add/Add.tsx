3b22c34467b1bfb962840ad8ea58546d557a305c
칼있으마 <bok-yeol.jung@doosan.com>
2025-01-07 09:49:42 +0900

    1:54:15 user랑 product까지는 다했음!

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -15,14 +15,14 @@ export const Add = (props:Props) => {
     }
   return (
     <div className='add'>
-        <div className="model">
+        <div className="modal">
             <span className="close" onClick={()=>props.setOpen(false)}>X</span>
             <h1>Add New {props.slug}</h1>
             <form action="" onSubmit={handleSubmit}>
                 {
                     props.columns
-                    .filter(item=>item.field !== "id" && item.field !== "img")
-                    .map(column=>(
+                    .filter((item) => item.field !== "id" && item.field !== "img")
+                    .map((column) => (
                         <div className="item">
                             <label htmlFor="">{column.headerName}</label>
                             <input type={column.type} placeholder={column.field} />
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -16,17 +16,6 @@ const columns: GridColDef<(typeof rows)[number]>[] = [
           return <img src={params.row.img || "/noavatar.png"} alt="" />
       },
   },
-  // {
-  //     field: 'actions',
-  //     headerName: 'Actions',
-  //     width: 100, 
-  //     renderCell(params) {
-  //         return <div className="action">
-  //             <div className="view">View</div>
-  //             <div className="delete">Delete</div>
-  //         </div>
-  //     },
-  // },
   {
     field: "firstName",
     type: "string",
@@ -64,19 +53,6 @@ const columns: GridColDef<(typeof rows)[number]>[] = [
     type: "boolean",
   },
 ];
-
-// const rows = [
-//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14, status:true },
-//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31, status:false },
-//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31, status:true },
-//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11, status:true },
-//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null, status:false },
-//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150, status:true },
-//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44, status:true },
-//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36, status:false },
-//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65, status:true },
-// ];
-
 
 export const Users = () => {
   const [open, setOpen] = useState(false);
